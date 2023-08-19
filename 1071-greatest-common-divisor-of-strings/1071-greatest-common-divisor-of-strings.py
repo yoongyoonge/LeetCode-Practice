10c718c1c0e904031ce070f030bbedd844b17d80
root <root@DESKTOP-RPEMCQ1.localdomain>
2023-08-19 22:25:23 +0900

    풀이 과정 메모 추가

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,8 +1,6 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         
-        ### 좋은 아이디어가 안떠올라서 다른사람 코드 복붙..
-        ### 반복되는지 체크한 후에 최소 공약수를 구해서 그만큼 문자열에서 추출
         # Check if concatenated strings are equal or not, if not return ""
         if str1 + str2 != str2 + str1:
             return ""
