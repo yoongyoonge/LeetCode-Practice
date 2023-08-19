82f644d880f99f62ad76a4dc8738446ad51529b7
yoongyoonge <namyunjin0912@gmail.com>
2023-08-19 21:45:04 +0900

    Time: 141 ms (96.08%), Space: 16.9 MB (21.73%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,51 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        # 예제 1
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
+        
+        # 예제 2
+        """
+        count = 0
+        for i in range(len(flowerbed)):
+            # Check if the current plot is empty.
+            if flowerbed[i] == 0:
+                # Check if the left and right plots are empty.
+                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
+                # If both plots are empty, we can plant a flower here.
+                if empty_left_plot and empty_right_lot:
+                    flowerbed[i] = 1
+                    count += 1
+                    
+        return count >= n
+        """
+    
+        # 예제 3
+        """
+        count = 0
+        for i in range(len(flowerbed)):
+            # Check if the current plot is empty.
+            if flowerbed[i] == 0:
+                # Check if the left and right plots are empty.
+                empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                
+                # If both plots are empty, we can plant a flower here.
+                if empty_left_plot and empty_right_lot:
+                    flowerbed[i] = 1
+                    count += 1
+                    if count >= n:
+                        return True
+                    
+        return count >= n
+        """
