c882d8a2a5fc5c847dade68ecae611918263a977
yoongyoonge <namyunjin0912@gmail.com>
2023-08-19 22:04:00 +0900

    Time: 145 ms (9.42%), Space: 18 MB (28.95%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -18,3 +18,25 @@ class Solution:
                 
         result = ''.join(tmp_s)
         return result
+    
+        # 예제 1
+        """
+        s = list(s)
+        left = 0
+        right = len(s) - 1
+        m = 'aeiouAEIOU'
+        while left < right:
+            if s[left] in m and s[right] in m:
+                
+                s[left], s[right] = s[right], s[left]
+                
+                left += 1; right -= 1
+            
+            elif s[left] not in m:
+                left += 1
+            
+            elif s[right] not in m:
+                right -= 1
+            
+        return ''.join(s)
+        """
