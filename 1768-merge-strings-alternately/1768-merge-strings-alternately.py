4017c85aa24a9ac11da31a0d2a77a9ca2e7a4586
yoongyoonge <namyunjin0912@gmail.com>
2023-08-19 19:32:26 +0900

    Time: 14 ms (76.90%), Space: 13.3 MB (49.59%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -6,6 +6,7 @@ class Solution(object):
         :rtype: str
         """
 
+        """
         result = ""
         iter_int = 0
         for i, j in zip(word1, word2):
@@ -14,5 +15,15 @@ class Solution(object):
 
         result = result + word1[iter_int:] + word2[iter_int:]
 
-
         return result
+        """
+    
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
