28064a60dd8b3a9d6272c71e7ed65be9766891da
yoongyoonge <namyunjin0912@gmail.com>
2023-08-20 22:36:03 +0900

    Time: 404 ms (51.82%), Space: 19.1 MB (23.62%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        
+        # 1004 번 문제 공부한 후 연계
         i = 0
         zero_cnt = 1
         for j in range(len(nums)):
@@ -14,4 +14,39 @@ class Solution:
                 
         
         return j-i
-            
+        
+        # 예제 1
+        """
+        longest = prev = curr = 0
+
+	    for bit in nums:
+	    	if bit:
+	    		curr += 1
+	    		longest = max(longest, prev + curr)
+	    	else:
+	    		prev, curr = curr, 0
+    
+	    return longest - (longest == len(nums))
+        """
+        
+        # 예제 2
+        """
+        n = len(nums)
+
+        left = 0
+        zeros = 0
+        ans = 0
+
+        for right in range(n):
+            if nums[right] == 0:
+                zeros += 1
+
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+
+            ans = max(ans, right - left + 1 - zeros)
+
+        return ans - 1 if ans == n else ans
+        """
