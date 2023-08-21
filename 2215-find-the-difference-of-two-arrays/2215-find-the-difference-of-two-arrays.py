57ff612144bece937500dd1f0bbdba70e4cd13b4
yoongyoonge <namyunjin0912@gmail.com>
2023-08-21 18:55:38 +0900

    Time: 162 ms (79.17%), Space: 16.6 MB (83.84%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -6,4 +6,23 @@ class Solution:
         
         return [list(nums1-nums2), list(nums2-nums1)]
         
+        # 예제 1
+        """
+        set1=set(nums1)
+        set2=set(nums2)
+        res=[[],[]]
+
+        for i in set1:
+            if i not in set2:
+                res[0].append(i)
+        for i in set2:
+            if i not in set1:
+                res[1].append(i)
+        return res
+        """
         
+        # 예제 2
+        """
+        set1,set2=set(nums1),set(nums2)
+        return[set1-set2,set2-set1]
+        """
