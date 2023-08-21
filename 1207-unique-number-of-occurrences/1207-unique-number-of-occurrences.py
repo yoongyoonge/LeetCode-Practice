d359dc6bb41067bc724d1eddda52b6637b089c82
yoongyoonge <namyunjin0912@gmail.com>
2023-08-21 20:11:28 +0900

    Time: 51 ms (47.35%), Space: 16.6 MB (14.96%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -3,13 +3,24 @@ class Solution:
         
         from collections import Counter
         
-        a = Counter(arr)
-       
-        a = sorted(a.values())
-        print(a)
+        a = sorted(Counter(arr).values())
         
         for i in range(0, len(a)-1):
             if a[i] == a[i+1]:
                 return False
                 
         return True
+    
+    # 예제 1
+    """
+    return (lambda x: len(x) == len(set(x)))(collections.Counter(A).values())
+    """
+    
+    # 예제 2
+    """
+    from collections import Counter
+    cnt = Counter(arr)     
+    x = cnt.values()
+    y = set(x)
+    return len(x) == len(y)
+    """
