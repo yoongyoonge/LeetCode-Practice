d054531ac7d9c22198f0bd052f546c830d41d0c8
yoongyoonge <namyunjin0912@gmail.com>
2023-08-21 21:19:10 +0900

    Time: 410 ms (98.03%), Space: 21.7 MB (32.36%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,14 +1,17 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         
+        # 성공하지만 성능이 좋지 않음
+        """
         cnt = 0
         for row in grid:
             for i in range(len(row)):
-                col = [c[i] for c in grid]
+                col = [c[i] for c in grid] 
                 if row == col:
                     cnt += 1
                     
         return cnt
+        """
     
         # 예제 1
         """
@@ -27,10 +30,10 @@ class Solution:
         """
         
         # 예제 2
-        """
-        tpse = Counter(zip(*grid))
-        grid = Counter(map(tuple,grid))
-        return  sum(tpse[t]*grid[t] for t in tpse)
-        """
+        
+        col = Counter(zip(*grid)) # 각 컬럼이 몇번 나타나는지 체크
+        row = Counter(map(tuple,grid)) # 각 행이 몇번 나타나는지 체크
+
+        return  sum(col[t]*row[t] for t in col) # 컬럼을 기준으로 해당 컬럼이 col과 row에서 몇번 나타나는지 페어로 체크후 개수를 반환
 
             
