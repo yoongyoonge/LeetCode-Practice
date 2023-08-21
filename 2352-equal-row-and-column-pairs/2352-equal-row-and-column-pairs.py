d9bb9444ebd13a1a56d89da032b5edb953ca4a2a
yoongyoonge <namyunjin0912@gmail.com>
2023-08-21 20:54:21 +0900

    Time: 2157 ms (9.10%), Space: 21.1 MB (94.75%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -5,13 +5,32 @@ class Solution:
         for row in grid:
             for i in range(len(row)):
                 col = [c[i] for c in grid]
-                
-                #print(row)
-                #print(col)
                 if row == col:
                     cnt += 1
                     
         return cnt
+    
+        # 예제 1
+        """
+        m = defaultdict(int)
+        cnt = 0
+
+        for row in grid:
+            m[str(row)] += 1
         
+        for i in range(len(grid[0])):
+            col = []
+            for j in range(len(grid)):
+                col.append(grid[j][i])
+            cnt += m[str(col)]
+        return cnt
+        """
+        
+        # 예제 2
+        """
+        tpse = Counter(zip(*grid))
+        grid = Counter(map(tuple,grid))
+        return  sum(tpse[t]*grid[t] for t in tpse)
+        """
 
             
