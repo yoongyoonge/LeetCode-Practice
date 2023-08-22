eb5592d2bf7f87057fd8609c70f62d2e0204385d
yoongyoonge <namyunjin0912@gmail.com>
2023-08-22 20:32:23 +0900

    Time: 292 ms (34.31%), Space: 18.4 MB (8.48%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -9,3 +9,25 @@ class Solution:
             else:
                 result.append(list_s[i])
         return ''.join(result)
+    
+        # 예제 1
+        """
+        ans=[]
+        for i in s:
+            if i=='*':
+                ans.pop()
+            else:
+                ans+=[i]
+        return "".join(ans)
+        """
+        
+        # 예제 2
+        """
+        res = []
+        for i in s:
+            if i != '*':
+                res.append(i)
+            elif res:
+                res.pop()
+        return ''.join(res)
+        """
