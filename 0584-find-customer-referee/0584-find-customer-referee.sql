fa4e9e5f55f754b6ff4a505d725241cad154f914
yoongyoonge <namyunjin0912@gmail.com>
2023-08-23 23:27:27 +0900

    Time: 1003 ms (72.22%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
-select name
-from Customer
-where referee_id != 2 or referee_id is null
+-- select name
+-- from Customer
+-- where referee_id != 2 or referee_id is null
+
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id,0) <> 2;
