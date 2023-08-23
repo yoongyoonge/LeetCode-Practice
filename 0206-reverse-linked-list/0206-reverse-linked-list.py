28545d8717daf37fa7fe8b819e14f80cc714de1e
yoongyoonge <namyunjin0912@gmail.com>
2023-08-23 23:19:28 +0900

    Time: 52 ms (35.02%), Space: 18.8 MB (15.86%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -23,4 +23,21 @@ class Solution:
         tmp_res = tmp
             
         return tmp_res
-            
+    
+        # 예제 1
+        """
+        # Initialize prev pointer as NULL...
+        prev = None
+        # Initialize the curr pointer as the head...
+        curr = head
+        # Run a loop till curr points to NULL...
+        while curr:
+            # Initialize next pointer as the next pointer of curr...
+            next = curr.next 
+            # Now assign the prev pointer to curr’s next pointer.
+            curr.next = prev
+            # Assign curr to prev, next to curr...
+            prev = curr 
+            curr = next 
+        return prev       # Return the prev pointer to get the reverse linked list...
+        """
