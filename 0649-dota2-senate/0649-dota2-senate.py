e1655c4be86a0e5f33f4e7772628705474668b79
yoongyoonge <namyunjin0912@gmail.com>
2023-08-23 22:06:49 +0900

    Time: 112 ms (31.85%), Space: 16.9 MB (7.94%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -18,15 +18,36 @@ class Solution:
         while len(rad_queue) != 0 and len(dir_queue) != 0:
             
             if rad_queue[0] < dir_queue[0]:
-                dir_queue.pop(0)
-                rad_queue.pop(0)
                 rad_queue.append(n)
-                n += 1
             else:
-                dir_queue.pop(0)
-                rad_queue.pop(0)
                 dir_queue.append(n)
-                n += 1
+
+            dir_queue.pop(0)
+            rad_queue.pop(0)
+            n += 1
 
         if len(rad_queue) > len(dir_queue): return "Radiant"
         else: return "Dire"
+        
+        # 예제 1
+        """
+        n = len(senate)
+        s, banned = set(), [False] * n
+        ban_d = ban_r = 0
+        while len(s) != 1:
+            s = set()
+            for i, p in enumerate(senate):
+                if banned[i]: continue
+                if p == 'R':
+                    if ban_r > 0:           # current R being banned
+                        ban_r -= 1
+                        banned[i] = True
+                    else:                   # if current R is valid, it will ban D
+                        ban_d += 1
+                        s.add('R')
+                else:        
+                    if ban_d > 0:           # current D being banned
+                        ban_d -= 1
+                        banned[i] = True
+                    else:                   # if current D is valid, it will ban R
+        """
