3cf15c4c43e3f5e130b70a6f7af3ee84494bd57a
yoongyoonge <namyunjin0912@gmail.com>
2023-08-24 20:45:42 +0900

    Time: 1310 ms (69.66%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -5,3 +5,4 @@ select
     , round(SUM(case when rating < 3 then 1 else 0 end)/COUNT(query_name)*100, 2) as poor_query_percentage
 from Queries
 group by query_name
+-- sum 이라서 else 0 써야하는데 까먹어서 뒤늦게 추가
