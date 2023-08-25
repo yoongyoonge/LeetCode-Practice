5ebc4f1963c87bc8bfd9cbda923babdabf053dcf
yoongyoonge <namyunjin0912@gmail.com>
2023-08-26 00:34:15 +0900

    Time: 2122 ms (39.48%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -14,3 +14,12 @@ inner join (
 ) min_base
 on s.product_id = min_base.product_id
 and s.year = min_base.min_year
+
+/* -- 다른 버전
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) IN (
+SELECT product_id, MIN(year) as year
+FROM Sales
+GROUP BY product_id) ;
+*/
