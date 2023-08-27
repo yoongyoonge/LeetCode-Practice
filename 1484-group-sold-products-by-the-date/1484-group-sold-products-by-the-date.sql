c5e457b142a04ffdf270573a579dea01d9c93140
yoongyoonge <namyunjin0912@gmail.com>
2023-08-28 00:37:30 +0900

    Time: 1154 ms (18.87%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -4,7 +4,7 @@
 select 
     sell_date
     , count(*) as num_sold
-    , group_concat(product order by product asc separator ',') as products
+    , group_concat(product order by product asc separator ',') as products -- group_concat을 처음 알게 되었다!
 from (
     select distinct sell_date, product
     from Activities a
