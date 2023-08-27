d337d7fac3f4c702351e921d4ec886069da8815f
yoongyoonge <namyunjin0912@gmail.com>
2023-08-27 23:25:24 +0900

    Time: 1773 ms (12.26%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -4,3 +4,11 @@ select
     , CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) as name
 from Users 
 order by user_id asc
+
+
+# 예제 1
+/*
+SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name,LENGTH(name)-1))) AS name
+FROM Users
+ORDER BY user_id ASC;
+*/
